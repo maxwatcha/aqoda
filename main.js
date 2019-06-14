2f759e01e3b46562b275d79c99ee664a0cb8e521
Peerapong Maitriwong <dev.peerapong@gmail.com>
2019-06-14 11:59:44 +0700

    received command from input file

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-function main() {
+const fs = require('fs')
+
+class Command {
+  constructor(name, params) {
+    this.name = name
+    this.params = params
+  }
 }
+
+function main() {
+  const filename = 'input.txt'
+  const commands = getCommandsFromFileName(filename)
+
+  console.log(commands)
+}
+
+function getCommandsFromFileName(fileName) {
+  const file = fs.readFileSync(fileName, 'utf-8')
+
+  return file
+    .split('\n')
+    .map(line => line.split(' '))
+    .map(([commandName, ...params]) => new Command(commandName, params))
+}
+
+main()
